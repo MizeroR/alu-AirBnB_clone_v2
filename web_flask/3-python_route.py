8fabd37e1bc2d1648c1dcc06191e47657c9639cd
MizeroR <r.mizero@alustudent.com>
2024-10-09 12:57:08 -0700

    updates

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,5 +23,11 @@ def echo(text):
     """Returns the string "C" followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
+
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
+    return 'Python ' + text.replace('_', ' ')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000'
