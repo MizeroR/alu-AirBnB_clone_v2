63031e9c9ab8c810ee5afc1672fe1491106c512c
MizeroR <r.mizero@alustudent.com>
2024-10-12 12:57:09 -0700

    updates

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -23,14 +23,19 @@ def c_text(text):
     """Returns the string "C" followed by the value of the text variable"""
     return 'C ' + text.replace('_', ' ')
 
-
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """ Function called with /python/<text> route """
+def pythoniscool(text='is cool'):
+    """display “Python ”, followed by the value of the text variable"""
     if text is not 'is cool':
         text = text.replace('_', ' ')
     return 'Python %s' % text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ Function called with /number/<n> route """
+    return "%d is a number" % n
 
 
 if __name__ == '__main__':
